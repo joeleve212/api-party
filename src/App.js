97c52e53a6ba918a18df7168f6f7a87873b976c2
unknown <Joe Leveille>
2018-07-10 11:42:39 -0400

    Add default route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {NavLink, Route, Switch} from 'react-router-dom'
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +12,19 @@ class App extends Component {
           <h3>It's an</h3>
           <h1>API Party</h1>
         </header>
+        
+        
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">Github API</NavLink>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/github" render={() => <h1>Hello Github!</h1>}/>
+          <Route render={() => (            //default route b/c no path
+            <p>Click a link to start!</p>
+          )}  />
+        </Switch>
       </div>
     );
   }
