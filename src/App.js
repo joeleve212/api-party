18aa29f1c70fea06de5a6f2ec82e8f8335a23639
unknown <Joe Leveille>
2018-07-10 20:46:41 -0400

    Update to Davey's class code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, NavLink, Switch } from 'react-router-dom'
 
-import {NavLink, Route, Switch} from 'react-router-dom'
+import './App.css'
+import Github from './Github'
+import Homework from './Homework'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <h3>It's an</h3>
+          <h3>Ain't no party like an</h3>
           <h1>API Party</h1>
         </header>
-        
-        
         <ul className="navLinks">
           <li>
-            <NavLink to="/github">Github API</NavLink>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+          <li>
+            <NavLink to="/homework">Homework</NavLink>
           </li>
         </ul>
         <Switch>
-          <Route path="/github" render={() => <h1>Hello Github!</h1>}/>
-          <Route render={() => (            //default route b/c no path
-            <p>Click a link to start!</p>
-          )}  />
+          <Route path="/github" component={Github} />
+          <Route path="/homework" component={Homework} />
+          <Route render={() => (
+              <p>To get started, click one of the links above.</p>
+          )} />
         </Switch>
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
